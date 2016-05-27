13d9015e5b9e701474a298fb80d8e527abf7faa5
Cian Johnston <johnstcn@gmail.com>
2016-05-27 20:28:26 +0100

    set session cookie age to somethimg more reasonable

--- whatsnew/settings.py
+++ whatsnew/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
-SESSION_COOKIE_AGE = 189216000000
+SESSION_COOKIE_AGE = 60 * 60 * 24 * 7
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = APP_CONFIG['secret_key']
