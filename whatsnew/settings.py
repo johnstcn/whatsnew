ad2252be4f5b3c210549122298160d7fc713073a
Cian Johnston <johnstcn@gmail.com>
2016-05-29 11:11:25 +0100

    dumb hacky way to get absolute url

--- whatsnew/settings.py
+++ whatsnew/settings.py
@@ -190,6 +190,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+# URL-building
+BASE_URL = APP_CONFIG.get('base_url', 'http://localhost:8000')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
